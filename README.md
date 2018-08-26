2cb236017a55a997b50e8f1cf2024dabd10fd7ea
wangpei07 <wangpei07@baidu.com>
2018-08-26 22:24:09 +0800

    add: promise && test && README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # promise
+
+用 es6 实现的一个个简单的 promise 版本
+
+## 安装依赖
+
+```
+npm i
+```
+
+## 编译 
+
+```
+npm run build
+
+// 编译产出在 lib 目录下
+```
+
+## 单元测试
+
+```
+npm run test
+
+// 直接利用第三方测试工具 promises-aplus-tests 进行单元测试
+```
---
+++ src/promise.js
@@ -0,0 +1,208 @@
+function Promise (executor) {
+    let self = this;
+    self.status = 'PENDING';
+    self.value = undefined;
+    self.reason = undefined;
+    self.onResolved = [];
+    self.onRejected = [];
+
+    function resolve(value) {
+        setTimeout(() => {
+            if (self.status === 'PENDING') {
+                self.status = 'RESOLVED';
+                self.value = value;
+                self.onResolved.forEach(fn => fn());
+            }
+        }, 0);
+    }
+
+    function reject(reason) {
+        setTimeout(() => {
+            if(self.status === 'PENDING') {
+                self.status = 'REJECTED';
+                self.reason = reason;
+                self.onRejected.forEach(fn => fn());
+            }
+        }, 0);
+    }
+
+    // 用于解决以下问题
+    // new Promise(() => throw Error('error))
+    try {
+        executor(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
+
+let resolutionProcedure = (promise2, x, resolve, reject) => {
+    // 2.3.1 
+    if (promise2 === x) {
+        return reject(new TypeError('循环引用'));
+    }
+
+    let called = false;
+    // 2.3.3
+    if (x != null && (typeof x === 'object' || typeof x === 'function')) {
+        try {
+            // 2.3.3.1
+            let then = x.then;
+            // 如果 then 是一个 function，则认为 x 是一个 promise
+            if (typeof then === 'function') {
+                // 2.3.3.3
+                then.call(x, y => {
+                    // 2.3.3.3.3
+                    if (called) return;
+                    called = true;
+                    // 2.3.3.3.1
+                    resolutionProcedure(promise2, y, resolve, reject)
+                }, r => {
+                    // 2.3.3.3.3
+                    if (called) return;
+                    called = true;
+                    reject(r);
+                });
+            }
+            else {
+                // 2.3.3.4
+                resolve(x);
+            }
+        } catch (e) {
+            // 2.3.3.3.3
+            if (called) return;
+            called = true;
+            // 2.3.3.2
+            reject(e);
+        }
+    }
+    else {
+        // 规范 2.3.4，x 为基本类型
+        resolve(x);
+    }
+}
+
+// 2.2 
+Promise.prototype.then = function (onResolved, onRejected) {
+    // 2.2.1
+    onResolved = typeof onResolved == 'function' ? onResolved : val => val;
+    onRejected = typeof onRejected == 'function' ? onRejected : err => {
+        throw err;
+    };
+
+    let self = this;
+
+    // 规范 2.2.7，then 必须返回一个新的 promise
+    let promise2;
+    promise2 = new Promise((resolve, reject) => {
+        if (self.status === 'RESOLVED') {
+            // 2.2.1.5 异步返回
+            setTimeout(() => {
+                try {
+                    let x = onResolved(self.value);
+                    resolutionProcedure(promise2, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            }, 0);
+        }
+
+        if (self.status === 'REJECTED') {
+            setTimeout(() => {
+                try {
+                    let x = onRejected(self.reason);
+                    resolutionProcedure(promise2, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            }, 0);
+        }
+
+        if (self.status === 'PENDING') {
+            self.onResolved.push(() => {
+                setTimeout(() => {
+                    try {
+                        let x = onResolved(self.value);
+                        resolutionProcedure(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                }, 0);
+            });
+
+            self.onRejected.push(() => {
+                setTimeout(() => {
+                    try {
+                        let x = onRejected(self.reason);
+                        resolutionProcedure(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                }, 0);
+            });
+        }
+    });
+    return promise2;
+}
+
+Promise.prototype.catch = function (onRejected) {
+  return this.then(null, onRejected);
+}
+
+Promise.all = (promises) => {
+    return new Promise((resolve, reject) => {
+        let sum = 0;
+        let results = [];
+        let processFn = (index, value) => {
+            results[index] = value;
+            sum++;
+            if (sum === promises.length) {
+                resolve(results);
+            }
+        };
+        for(let index = 0; index < promises.length; index++) {
+            let p = promises[i];
+            p.then((value) => {
+                processFn(index, value);
+            }, reject);
+        }
+    });
+};
+
+Promise.race = (promises) => {
+    return new Promise((resolve, reject) => {
+        for(let index = 0; index < promises.length; index++) {
+            let p = promises[index];
+            p.then((value) => {
+                resolve(value);
+            }, reject);
+        }
+    });
+}
+
+Promise.resolve = (value) => {
+    return new Promise((resolve, reject) => {
+        resolve(value);
+    });
+}
+
+Promise.reject = (reason) => {
+    return new Promise((resolve, reject) => {
+        reject(reason);
+    });
+}
+
+Promise.defer = Promise.deferred = function () {
+  let dfd = {};
+  dfd.promise = new Promise((resolve, reject) => {
+    dfd.resolve = resolve;
+    dfd.reject = reject;
+  });
+  return dfd;
+}
+
+module.exports = Promise
+
+
+
+
+
